e8ee0f621bfc5e9857c26814dc3b059f503ddc9f
gsucharitha2005 <gsucharitha2005@gmail.com>
2026-06-30 14:24:13 +0530

    writing sum of two numbers in functions

---
+++ sum_of_two_numbers.py
@@ -0,0 +1,22 @@
+def sum(a,b):
+	c=a+b
+	print("sum of two numbers:",c)
+sum(10,20)
+
+def sum(a,b):
+	c=a+b
+	print("sum of two numbers:",c)
+sum("amma","nanna")
+
+
+def sum(a,b):
+	c=a+b
+	print("sum of two numbers:",c)
+sum(["python"],["intern"])
+
+
+
+def sum(a,b):
+	c=a+b
+	print("sum of two numbers:",c)
+sum(("python"),("intern"))
