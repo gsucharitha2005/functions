9f5428761c8c9d04fb62441178d915e79e2a3675
gsucharitha2005 <gsucharitha2005@gmail.com>
2026-07-01 11:51:07 +0530

    checking admission eligibility

---
+++ admission_eligibility.py
@@ -0,0 +1,7 @@
+def check_admission_eligibility(age,location):
+	if age>=10 and age<=12 and location=="rural":
+		return True
+	else:
+		return False
+print(check_admission_eligibility(12,"rural"))
+print(check_admission_eligibility(10,"urban"))
